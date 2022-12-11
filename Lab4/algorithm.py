ade8c2b27cacfa0cd24e29924445c995e3a205c7
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-12-11 14:11:46 +0200

    add func to find available color, some other chgs

--- Lab4/algorithm.py
+++ Lab4/algorithm.py
@@ -1,15 +1,30 @@
+"""
+Задача розфарбовування графу (100 вершин, степінь вершини не більше 20, 
+але не менше 1), класичний бджолиний алгоритм (число бджіл 30 із них 
+3 розвідники).
+"""
+
 class GraphColoring:
-    def __init__(self, path):
+    def __init__(self, path: str):
         with open(path, 'r') as f:
             lines = f.readlines()
             self.num = int(lines[0])
             lines = lines[1:]
             self.edges = [list(map(int, line.split())) for line in lines]
+        self.remove_duplicate_edges()
     
-    def get_neighbors(self, node):
+    def remove_duplicate_edges(self):
+        lst = []
+        for start, end in self.edges:
+            if start != end:
+                if ([start, end] not in lst) and ([end, start] not in lst):
+                    lst.append([start, end])
+        self.edges = lst
+    
+    def get_neighbors(self, vertex: int):
         neighbors = []
         for start, end in self.edges:
-            if start == node:
+            if start == vertex:
                 neighbors.append(end)
         return neighbors
 
@@ -22,5 +37,15 @@ class GraphColoring:
                 max_count = count
                 max_vertex = vertex
         return max_vertex
-
     
+    def get_available_color(self, vertex: int, vertex_colors: list[int], num_of_colors: int):
+        neighbors = self.get_neighbors(vertex)
+        available_colors = [color for color in range(num_of_colors)]
+        if vertex_colors[vertex] in available_colors:
+            available_colors.remove(vertex_colors[vertex])
+        for neighbor in neighbors:
+            if vertex_colors[neighbor] in available_colors:
+                available_colors.remove(vertex_colors[neighbor])
+        if len(available_colors) == 0:
+            return -1
+        return available_colors[0]
