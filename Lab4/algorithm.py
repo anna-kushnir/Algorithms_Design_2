9f3f12418fde4ea45bd40f279a8e8c3b3e115f0e
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-12-11 02:53:18 +0200

    def func to get vertex with highest multiplicity

--- Lab4/algorithm.py
+++ Lab4/algorithm.py
@@ -5,3 +5,22 @@ class GraphColoring:
             self.num = int(lines[0])
             lines = lines[1:]
             self.edges = [list(map(int, line.split())) for line in lines]
+    
+    def get_neighbors(self, node):
+        neighbors = []
+        for start, end in self.edges:
+            if start == node:
+                neighbors.append(end)
+        return neighbors
+
+    def get_vertex_with_highest_multiplicity(self):
+        max_vertex = 0
+        max_count = 0
+        for vertex, odd in set(self.edges):
+            count = self.edges.count(vertex)
+            if count > max_count:
+                max_count = count
+                max_vertex = vertex
+        return max_vertex
+
+    
