40e635b6e89762b9d54877c6de48c7fc85f0f87e
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-12-12 03:31:25 +0200

    changed creation of graph

--- Lab4/creation.py
+++ Lab4/creation.py
@@ -1,18 +1,34 @@
 import numpy as np
 
 class CreateGraph:
-    def __init__(self, num = 100, min_pow = 1, max_pow = 20):
+    def __init__(self, num: int = 100, min_pow: int = 1, max_pow: int = 20):
         self.num = num
         self.min_pow = min_pow
         self.max_pow = max_pow
 
-    def create(self, path):
+    def create_and_save_to_file(self, path: str):
+        self._generate_graph()
         with open(path, 'w') as f:
             f.write(str(self.num) + '\n')
-            for i in range(self.num):
-                num_of_neighbors = np.random.randint(1, self.max_pow + 1)
-                all_vertices = np.arange(self.num)
-                np.random.shuffle(all_vertices)
-                neighboring_vertices = all_vertices[:num_of_neighbors]
-                for neighbor in neighboring_vertices:
-                    f.write(str(i) + ' ' + str(neighbor) + '\n')
+            for edge in self.edges:
+                f.write(str(edge[0]) + ' ' + str(edge[1]) + '\n')
+    
+    def _generate_graph(self):
+        self.edges = []
+        counts = [0 for i in range(self.num)]
+        for vertex in range(self.num):
+            num_of_neighbors = np.random.randint(1, self.max_pow)
+            if counts[vertex] + num_of_neighbors > self.max_pow:
+                num_of_neighbors = self.max_pow - counts[vertex]
+            counts[vertex] += num_of_neighbors
+            i = 0
+            neighbors = []
+            while i < num_of_neighbors:
+                neighbor = np.random.randint(0, self.num)
+                if vertex != neighbor and ([vertex, neighbor] not in self.edges) and ([neighbor, vertex] not in self.edges):
+                    if counts[neighbor] < self.max_pow:
+                        neighbors.append(neighbor)
+                        counts[neighbor] += 1
+                        i += 1
+            for neighbor in neighbors:
+                self.edges.append([vertex, neighbor])
--- Lab4/main.py
+++ Lab4/main.py
@@ -1,5 +1,7 @@
 from creation import *
+from algorithm import *
 
 if __name__ == "__main__":
+    path = 'file_lab4.txt'
     f = CreateGraph()
-    f.create('file_lab4.txt')
+    f.create_and_save_to_file(path)
