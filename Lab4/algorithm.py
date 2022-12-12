31f0aa20776a286b7c67a99814baf6b50f8bba8a
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-12-12 03:32:12 +0200

    greedy algorithm was added and func to draw graph

--- Lab4/algorithm.py
+++ Lab4/algorithm.py
@@ -1,3 +1,7 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
 """
 Задача розфарбовування графу (100 вершин, степінь вершини не більше 20, 
 але не менше 1), класичний бджолиний алгоритм (число бджіл 30 із них 
@@ -6,40 +10,36 @@
 
 class GraphColoring:
     def __init__(self, path: str):
+        self.scout_bees = 3
+        self.foragers = 30
         with open(path, 'r') as f:
             lines = f.readlines()
             self.num = int(lines[0])
             lines = lines[1:]
             self.edges = [list(map(int, line.split())) for line in lines]
-        self.remove_duplicate_edges()
     
-    def remove_duplicate_edges(self):
-        lst = []
-        for start, end in self.edges:
-            if start != end:
-                if ([start, end] not in lst) and ([end, start] not in lst):
-                    lst.append([start, end])
-        self.edges = lst
-    
-    def get_neighbors(self, vertex: int):
+    def _get_neighbors(self, vertex: int):
         neighbors = []
         for start, end in self.edges:
             if start == vertex:
                 neighbors.append(end)
-        return neighbors
+            if end == vertex:
+                neighbors.append(start)
+        return set(neighbors)
 
-    def get_vertex_with_highest_multiplicity(self):
+    def _get_vertex_with_highest_multiplicity(self):
         max_vertex = 0
         max_count = 0
-        for vertex, odd in set(self.edges):
-            count = self.edges.count(vertex)
+        vertices_in_edges = [start for start, end in self.edges] + [end for start, end in self.edges]
+        for vertex in set(vertices_in_edges):
+            count = vertices_in_edges.count(vertex)
             if count > max_count:
                 max_count = count
                 max_vertex = vertex
         return max_vertex
     
-    def get_available_color(self, vertex: int, vertex_colors: list[int], num_of_colors: int):
-        neighbors = self.get_neighbors(vertex)
+    def _get_available_color(self, vertex: int, num_of_colors: int, vertex_colors: list[int]):
+        neighbors = self._get_neighbors(vertex)
         available_colors = [color for color in range(num_of_colors)]
         if vertex_colors[vertex] in available_colors:
             available_colors.remove(vertex_colors[vertex])
@@ -49,3 +49,38 @@ class GraphColoring:
         if len(available_colors) == 0:
             return -1
         return available_colors[0]
+    
+    def _is_color_available(self, vertex: int, color: int, vertex_colors: list[int]):
+        neighbors = self._get_neighbors(vertex)
+        for neighbor in neighbors:
+            if vertex_colors[neighbor] == color:
+                return False
+        return True
+
+    def greedy_algorithm(self):
+        self.vertex_colors = [-1 for i in range(self.num)]
+        curr_color = 0
+        while -1 in self.vertex_colors:
+            for vertex in range(self.num):
+                if self.vertex_colors[vertex] == -1:
+                    if self._is_color_available(vertex, curr_color, self.vertex_colors):
+                        self.vertex_colors[vertex] = curr_color
+            curr_color += 1
+        self.draw_graph()
+        return curr_color
+
+    def bee_algorithm(self):
+        num_of_colors = self.greedy_algorithm()
+        self.draw_graph()
+
+    def draw_graph(self):
+        print('Number of colors:', len(set(self.vertex_colors)))
+        print('Colors of vertices:', self.vertex_colors)
+        graph = nx.Graph()
+        for u, v in self.edges:
+            graph.add_edge(u, v)
+        pos = nx.spring_layout(graph)
+        colors = ['red', 'magenta', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple', 'pink', 'brown', 'grey', 'black']
+        vertex_colors_names = [colors[self.vertex_colors[vertex]] for vertex in graph.nodes()]
+        nx.draw(graph, pos, with_labels = True, node_color = vertex_colors_names, edge_color = 'black', alpha = 0.7)
+        plt.show()
--- Lab4/main.py
+++ Lab4/main.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     path = 'file_lab4.txt'
     f = CreateGraph()
     f.create_and_save_to_file(path)
+    algorithm = GraphColoring(path)
+    algorithm.bee_algorithm()
