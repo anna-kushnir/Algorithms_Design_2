2c76fa1d73cd3e7841a6d615b75f130299412d31
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2023-01-14 15:14:13 +0200

    code cleanup

--- lab5/main.py
+++ lab5/main.py
@@ -3,12 +3,11 @@ from algorithm import *
 
 if __name__ == "__main__":
     path = 'file_lab5_test2.txt'
-    f = CreateGraph()
-    f.create_and_save_to_file(path)
+    # f = CreateGraph()
+    # f.create_and_save_to_file(path)
     scout_bees = 3
     foragers = 60
     solutions = 1
-    iterations = 1000
+    iterations = 100
     algorithm = Algorithm(path, scout_bees, foragers, solutions, iterations)
     algorithm.bee_algorithm()
-    
